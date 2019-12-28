18aab2ccb6b4cfb8b69490efca88296e79f9c327
Harshveer Singh <harshveer1889@gmail.com>
2019-12-28 20:30:46 +0530

    Add title plugin

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -27,7 +27,7 @@ import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
-// import Title from '@ckeditor/ckeditor5-heading/src/title';
+import Title from '@ckeditor/ckeditor5-heading/src/title';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
 import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
@@ -55,6 +55,7 @@ class DocumentEditor extends DocumentEditorBase {}
 const plugins = [
 	Essentials,
 	PasteFromOffice,
+	Title,
 	UploadAdapter,
 	Autoformat,
 	Bold,
