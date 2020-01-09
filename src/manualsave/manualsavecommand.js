fa833f6c368e768133b931f75af1265f2ed8e692
Harshveer Singh <harshveer.singh@careerbuilder.com>
2020-01-09 17:09:04 +0530

    UPdate

--- src/manualsave/manualsavecommand.js
+++ src/manualsave/manualsavecommand.js
@@ -2,12 +2,17 @@ import Command from '@ckeditor/ckeditor5-core/src/command';
 
 export default class ManualSaveCommand extends Command {
     execute(isSave) {
-        var manualSaveConfig = this.editor.config.manualSave;
+        var manualSaveConfig = this.editor.config.manualSave || this.editor.config.get('manualSave');
 
-        if (isSave) {
-            manualSaveConfig.saveClicked();
+        if (manualSaveConfig) {
+            if (isSave) {
+                manualSaveConfig.saveClicked();
+            } else {
+                manualSaveConfig.cancelClicked();
+            }
         } else {
-            manualSaveConfig.cancelClicked();
+            console.log('Config for manual save is not defined.');
+            console.log(this.editor.config);
         }
     }
 
